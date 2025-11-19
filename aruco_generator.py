16b51a5413b9d6cdf85a6f7efe6dd614baddf3c5
i3l3 <gdhan22@hints.or.kr>
2025-11-20 01:11:28 +0900

    smaller marker

--- aruco_generator.py
+++ aruco_generator.py
@@ -2,13 +2,13 @@ import cv2
 import cv2.aruco as aruco
 import random
 
-dictionary = aruco.getPredefinedDictionary(aruco.DICT_5X5_1000)
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
 marker_amount = 2
 marker_ids = []
 marker_size = 300
 for i in range(marker_amount):
-    marker_id = random.randint(0, 999)
+    marker_id = random.randint(0, 49)
     if marker_id in marker_ids:
         continue
     marker_ids.append(marker_id)
