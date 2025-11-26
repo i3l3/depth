f44ffc81e63579aa8c0cb1b445e4b590a4acdf01
i3l3 <gdhan22@hints.or.kr>
2025-11-26 10:08:05 +0900

    fin

--- obj_detection.py
+++ obj_detection.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO
 import numpy as np
-import cv2.aruco as aruco
 import cv2
 import torch
 import time
@@ -17,43 +16,20 @@ midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 transform = midas_transforms.small_transform
 
 depth_value = 0
-real_depth_value = 0
 mouse_x, mouse_y = 0, 0
 def show_depth(event, x, y, _, param):
-    global depth_value, real_depth_value, mouse_x, mouse_y
+    global depth_value, mouse_x, mouse_y
     if event == cv2.EVENT_MOUSEMOVE:
         mouse_x, mouse_y = x, y
         depth_value = param[y, x]
-        real_depth_value = 1 / (a * depth_value + b)
 
 cv2.namedWindow("depth")
-
-# ArUco markers
-dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-markers = {} # id: depth (cm)
-a, b = 0, 0 # scale factors
-for _ in range(2):
-    marker = input("marker_id marker_depth (cm): ")
-    if marker == "":
-        break
-    markers[int(marker.split()[0])] = int(marker.split()[1])
-
-def get_registered(_ids, _markers, _corners):
-    registered_markers = []
-    registered_corners = []
-    if _ids is None:
-        return registered_markers, registered_corners
-    for i, _id in enumerate(_ids):
-        if _id[0] in _markers:
-            registered_markers.append([_id[0]])
-            registered_corners.append(_corners[i])
-    print((registered_markers, registered_corners))
-    return registered_markers, registered_corners # ([[0], [1], ...], [corner0, corner1, ...])
-
 
 # YOLO
 model = YOLO('models/yolo11s-seg.pt')
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 prev_crops = set()
 
@@ -91,13 +67,6 @@ while True:
     depth_map_vis = (depth_map_norm * 255).astype(np.uint8)
     depth_map_vis = cv2.applyColorMap(depth_map_vis, cv2.COLORMAP_MAGMA)
 
-    # ArUco
-    corners, ids, rejected = aruco.detectMarkers(gray, dictionary)
-    keys, corners = get_registered(ids, markers, corners)
-    aruco.drawDetectedMarkers(frame, corners, np.array(keys))
-    centers = [(int(corner[:, 0].mean()), int(corner[:, 1].mean())) for corner in corners]
-    ...
-
     # FPS
     end = time.time()
     totalTime = end - start
@@ -106,7 +75,7 @@ while True:
     # Depth info
     cv2.putText(
         depth_map_vis,
-        f"depth_norm: {depth_value:.3f} depth: {real_depth_value:.3f} fps: {fps:.1f}",
+        f"depth: {depth_value:.3f} fps: {fps:.1f}",
         (10, 30),
         cv2.FONT_HERSHEY_SIMPLEX,
         1,
